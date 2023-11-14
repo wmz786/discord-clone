b18e88d139bab85152871b119b6eee57a622ec5d
wmz786 <waismalikzada9@gmail.com>
2023-11-14 13:39:35 +0430

    add sign-in sign-up clerk package

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="bg-gray-600 h-full">{children}</div>;
+};
+
+export default AuthLayout;
---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col">
+      <p className="text-3xl text-center font-bold text-indigo-500">
+        This page is protected
+      </p>
+    </div>
+  );
+}
