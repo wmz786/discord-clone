fe295ab6ba8e1d12ebe4670c8e8f195339e8a328
wmz786 <waismalikzada9@gmail.com>
2023-11-16 12:47:16 +0430

    add dark mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <p className="text-3xl text-center font-bold text-indigo-500">
-        This page is protected
-      </p>
+    <div>
+      Home Page
+      {/* <UserButton afterSignOutUrl="/" /> */}
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -16,10 +17,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>{children}</body>
-      </html>
-    </ClerkProvider>
+    // <ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body className={font.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem={false}
+          storageKey="discord-theme"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+    // </ClerkProvider>
   );
 }
